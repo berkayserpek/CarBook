975d2713b182fcbd7e37cdedc77bfd89d5bd058d
Berkay-Serpek <berkayserpek@gmail.com>
2024-03-02 06:40:19 +0300

    about api added

--- Presentation/CarBook.API/Program.cs
+++ Presentation/CarBook.API/Program.cs
@@ -1,6 +1,18 @@
+using CarBook.Persistance.Context;
+using CarBook.Persistance.Repositories;
+using CarBookApplication.Features.CQRS.Handlers.AboutHandlers;
+using CarBookApplication.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<CarBookContext>();
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+builder.Services.AddScoped<GetAboutQueryHandler>();
+builder.Services.AddScoped<GetAboutByIdQueryHandler>();
+builder.Services.AddScoped<CreateAboutCommandHandler>();
+builder.Services.AddScoped<UpdateAboutCommandHandler>();
+builder.Services.AddScoped<RemoveAboutCommandHandler>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
