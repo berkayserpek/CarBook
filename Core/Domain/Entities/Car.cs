9f3a3bdf1da609d4af51e77069b1a16d277ae116
Berkay-Serpek <berkayserpek@gmail.com>
2024-03-02 05:43:42 +0300

    Add project files.

---
+++ Core/Domain/Entities/Car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Car
+    {
+        public int ID { get; set; }
+        public int BrandID { get; set; }
+        public Brand Brand { get; set; }
+        public string Model { get; set; }
+        public string CoverImageURL { get; set; }
+        public int KM { get; set; }
+        public string Transmission { get; set; }
+        public byte Seat { get; set; }
+        public byte Luggage { get; set; }
+        public string Fuel { get; set; }
+        public string BigImageURL { get; set; }
+        public List<CarFeature> CarFeatures { get; set; }
+        public List<CarDescription> CarDescriptions { get; set; }
+        public List<CarPricing> CarPricings { get; set; }
+
+    }
+}
