f16962a312655dcd5b8158b0afd0f85d1184f6d7
Berkay-Serpek <berkayserpek@gmail.com>
2024-03-07 22:29:28 +0300

    about area consume added

---
+++ Frontends/CarBook.DTO/AboutDTO/ResultAboutDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.DTO.AboutDTO
+{
+    public class ResultAboutDTO
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ImageURL { get; set; }
+    }
+}
