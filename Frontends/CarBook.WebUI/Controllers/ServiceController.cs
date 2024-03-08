cf572a63b386d2283384c073e3504020694531e9
Berkay-Serpek <berkayserpek@gmail.com>
2024-03-08 19:56:32 +0300

    Car and contact page added

---
+++ Frontends/CarBook.WebUI/Controllers/ServiceController.cs
@@ -0,0 +1,29 @@
+﻿using CarBook.DTO.ServiceDTO;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarBook.WebUI.Controllers
+{
+    public class ServiceController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ServiceController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7070/api/Services");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var values = await responseMessage.Content.ReadAsStringAsync();
+                var jsonData = JsonConvert.DeserializeObject<List<ResultServiceDTO>>(values);
+                return View(jsonData);
+            }
+            return View();
+        }
+    }
+}
