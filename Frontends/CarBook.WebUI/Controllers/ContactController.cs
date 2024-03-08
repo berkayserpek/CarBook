68eae93cea258b4e8e2cb5c4a1178323a1bff9f8
Berkay-Serpek <berkayserpek@gmail.com>
2024-03-08 21:09:27 +0300

    Contact post added

--- Frontends/CarBook.WebUI/Controllers/ContactController.cs
+++ Frontends/CarBook.WebUI/Controllers/ContactController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarBook.DTO.ContactDTO;
+using Microsoft.AspNetCore.Http.Metadata;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace CarBook.WebUI.Controllers
 {
@@ -11,8 +15,23 @@ namespace CarBook.WebUI.Controllers
             _httpClientFactory = httpClientFactory;
         }
 
+        [HttpGet]
         public IActionResult Index()
         {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(CreateContactDTO createContactDTO)
+        {
+            var client = _httpClientFactory.CreateClient();
+            createContactDTO.SendDate = DateTime.Now;
+            var jsonData = JsonConvert.SerializeObject(createContactDTO);
+            StringContent stringContent = new StringContent(jsonData,Encoding.UTF8,"application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7070/api/Contacts", stringContent);
+            if(responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Default");
+            }
             return View();
         }
     }
